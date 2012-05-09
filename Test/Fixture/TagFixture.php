c837f1b3bbe3c75385626c45da9045d478122791
Richard Kersey <github@razorit.com>
2012-05-08 21:14:57 -0400

    Updated Fixture to use Zuha Import Config Schema Method.
    Instead of using the fixture itself to define the schema we use the schema files to define the schema so that there it is only needed in one place.

--- Test/Fixture/TagFixture.php
+++ Test/Fixture/TagFixture.php
@@ -31,25 +31,13 @@ class TagFixture extends CakeTestFixture {
  */
 	public $table = 'tags';
 
+	
 /**
- * Fields
+ * Import
  *
- * @var array $fields
+ * @var array
  */
-	public $fields = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
-		'identifier' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 30, 'key' => 'index'),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 30),
-		'keyname' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 30),
-		'occurrence' => array('type' => 'integer', 'null' => false, 'default' => 0, 'length' => 8),
-		'article_occurrence' => array('type' => 'integer', 'null' => false, 'default' => 0, 'length' => 8),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'UNIQUE_TAG' => array('column' => array('identifier', 'keyname'), 'unique' => 1)
-		)
-	);
+	public $import = array('config' => 'Tags.Tag');
 
 /**
  * Records
--- Test/Fixture/TaggedFixture.php
+++ Test/Fixture/TaggedFixture.php
@@ -30,28 +30,13 @@ class TaggedFixture extends CakeTestFixture {
  * @var string name$table
  */
 	public $table = 'tagged';
-
+	
 /**
- * Fields
+ * Import
  *
- * @var array $fields
+ * @var array
  */
-	public $fields = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
-		'foreign_key' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
-		'tag_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
-		'model' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index'),
-		'language' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 6),
-		'times_tagged' => array('type' => 'integer', 'null' => false, 'default' => 1),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'UNIQUE_TAGGING' => array('column' => array('model', 'foreign_key', 'tag_id', 'language'), 'unique' => 1),
-			'INDEX_TAGGED' => array('column' => 'model', 'unique' => 0),
-			'INDEX_LANGUAGE' => array('column' => 'language', 'unique' => 0)
-		)
-	);
+	public $import = array('config' => 'Tags.Tag');
 
 /**
  * Records
